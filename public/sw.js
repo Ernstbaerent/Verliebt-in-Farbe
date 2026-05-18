e4dff4dc757a179b831bbfb009b4ac4876f28b91
Ernstbaerent <clemens.huth2013@gmail.com>
2026-05-18 20:02:10 +0200

    feat: Implement basic PWA functionality
    This commit introduces foundational Progressive Web App (PWA) features by setting up a service worker and configuring routing for client-side navigation.
    
    The service worker is simplified to enable caching and offline capabilities, while the `.htaccess` file is updated to handle client-side routing, ensuring that all requests are directed to `index.html` for proper application handling.
    
    The `index.html` is updated to unregister old service workers, ensuring a clean state for the new PWA setup.

--- public/sw.js
+++ public/sw.js
@@ -1,48 +1,19 @@
-var CACHE_NAME = 'pwa-task-manager';
-var urlsToCache = [
-  '/',
-  '/index.html'
-];
-
-// Install a service worker
-self.addEventListener('install', event => {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-// Cache and return requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-// Update a service worker
-self.addEventListener('activate', event => {
-  var cacheWhitelist = ['pwa-task-manager'];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
+        cacheNames.map((cacheName) => caches.delete(cacheName))
       );
+    }).then(() => {
+      return self.clients.claim();
     })
   );
+});
+
+self.addEventListener('fetch', (e) => {
+  // Pass through everything
 });
